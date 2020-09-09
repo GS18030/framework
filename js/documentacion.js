2344b8daa93bd95e23482d3283140a139cafb460
Ivan Romero <ivanrmrhdz@gmail.com>
2020-09-09 07:15:07 -0600

    aa
    aa

--- js/documentacion.js
+++ js/documentacion.js
@@ -1,22 +1,30 @@
-document.getElementById("layout").addEventListener("click", visualizarLayout);
+document.getElementById("docu").addEventListener("click", visualizarLayout);
+document.getElementById("docu2").addEventListener("click", visualizarLayout);
+
 document.getElementById("tipografia").addEventListener("click", visualizarTipografia);
 document.getElementById("utilidades").addEventListener("click", visualizarUtilidades);
 document.getElementById("componentes").addEventListener("click", visualizarComponentes);
 
 function visualizarLayout(){
-    document.getElementById('contenido1').classList.add('activo');
-    document.getElementById('contenido1').classList.remove('desactivo');
+
     
-    document.getElementById('contenido2').classList.add('desactivo');
-    document.getElementById('contenido2').classList.remove('activo');
+    if(document.getElementById('menuizquierdo').classList.contains('oculto')){
+        document.getElementById('menuizquierdo').classList.remove('oculto');
+        document.getElementById('contenido').classList.remove('w-100');
+        document.getElementById('contenido').classList.add('w-75');
+       
 
-    document.getElementById('contenido3').classList.add('desactivo');
-    document.getElementById('contenido3').classList.remove('activo');
+    } else{
 
-    document.getElementById('contenido4').classList.add('desactivo');
-    document.getElementById('contenido4').classList.remove('activo');
+        document.getElementById('menuizquierdo').classList.add('oculto');
 
-   
+        document.getElementById('contenido').classList.remove('w-75');
+        document.getElementById('contenido').classList.add('w-100');
+    }
+
+  
+
+    
 }
 
 
@@ -67,4 +75,12 @@ function visualizarComponentes(){
   
 }
 
+document.getElementById("docu").addEventListener("click", mostrarmenu);
 
+
+
+
+function mostrarmenu(){
+
+    
+}
