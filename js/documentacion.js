81bad8c916ec9f488f9fe5f20cc5d1ae53580f39
CoreasMarlon <marlon.coreasv@gmail.com>
2020-09-10 23:02:22 -0600

    Actualizacion Chele

--- js/documentacion.js
+++ js/documentacion.js
@@ -1,70 +1,26 @@
-document.getElementById("layout").addEventListener("click", visualizarLayout);
-document.getElementById("tipografia").addEventListener("click", visualizarTipografia);
-document.getElementById("utilidades").addEventListener("click", visualizarUtilidades);
-document.getElementById("componentes").addEventListener("click", visualizarComponentes);
+document.getElementById("docu").addEventListener("click", visualizarLayout);
+document.getElementById("docu2").addEventListener("click", visualizarLayout);
+
+
 
 function visualizarLayout(){
-    document.getElementById('contenido1').classList.add('activo');
-    document.getElementById('contenido1').classList.remove('desactivo');
+
     
-    document.getElementById('contenido2').classList.add('desactivo');
-    document.getElementById('contenido2').classList.remove('activo');
+    if(document.getElementById('menuizquierdo').classList.contains('oculto')){
+        document.getElementById('menuizquierdo').classList.remove('oculto');
+        document.getElementById('contenido').classList.remove('w-100');
+        document.getElementById('contenido').classList.add('w-75');
+       
 
-    document.getElementById('contenido3').classList.add('desactivo');
-    document.getElementById('contenido3').classList.remove('activo');
+    } else{
 
-    document.getElementById('contenido4').classList.add('desactivo');
-    document.getElementById('contenido4').classList.remove('activo');
+        document.getElementById('menuizquierdo').classList.add('oculto');
 
-   
-}
-
-
-function visualizarTipografia(){
-    document.getElementById('contenido1').classList.add('desactivo');
-    document.getElementById('contenido1').classList.remove('activo');
-    
-    document.getElementById('contenido2').classList.add('activo');
-    document.getElementById('contenido2').classList.remove('desactivo');
-
-    document.getElementById('contenido3').classList.add('desactivo');
-    document.getElementById('contenido3').classList.remove('activo');
-
-    document.getElementById('contenido4').classList.add('desactivo');
-    document.getElementById('contenido4').classList.remove('activo');
-
-   
-}
-
-function visualizarUtilidades(){
-    document.getElementById('contenido1').classList.add('desactivo');
-    document.getElementById('contenido1').classList.remove('activo');
-    
-    document.getElementById('contenido2').classList.add('desactivo');
-    document.getElementById('contenido2').classList.remove('activo');
-
-    document.getElementById('contenido3').classList.add('activo');
-    document.getElementById('contenido3').classList.remove('desactivo');
-
-    document.getElementById('contenido4').classList.add('desactivo');
-    document.getElementById('contenido4').classList.remove('activo');
-
-}
-
-function visualizarComponentes(){
-    document.getElementById('contenido1').classList.add('desactivo');
-    document.getElementById('contenido1').classList.remove('activo');
-    
-    document.getElementById('contenido2').classList.add('desactivo');
-    document.getElementById('contenido2').classList.remove('activo');
-
-    document.getElementById('contenido3').classList.add('desactivo');
-    document.getElementById('contenido3').classList.remove('activo');
-
-    document.getElementById('contenido4').classList.add('activo');
-    document.getElementById('contenido4').classList.remove('desactivo');
+        document.getElementById('contenido').classList.remove('w-75');
+        document.getElementById('contenido').classList.add('w-100');
+    }
 
   
+
+    
 }
-
-
