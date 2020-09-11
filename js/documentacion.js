f70b934e0e65d8ce534a3ade261b2348c1c18edd
CoreasMarlon <marlon.coreasv@gmail.com>
2020-09-11 01:27:37 -0600

    Indentacion

--- js/documentacion.js
+++ js/documentacion.js
@@ -1,26 +1,14 @@
 document.getElementById("docu").addEventListener("click", visualizarLayout);
 document.getElementById("docu2").addEventListener("click", visualizarLayout);
 
-
-
-function visualizarLayout(){
-
-    
-    if(document.getElementById('menuizquierdo').classList.contains('oculto')){
+function visualizarLayout() {
+    if (document.getElementById('menuizquierdo').classList.contains('oculto')) {
         document.getElementById('menuizquierdo').classList.remove('oculto');
         document.getElementById('contenido').classList.remove('w-100');
         document.getElementById('contenido').classList.add('w-75');
-       
-
-    } else{
-
+    } else {
         document.getElementById('menuizquierdo').classList.add('oculto');
-
         document.getElementById('contenido').classList.remove('w-75');
         document.getElementById('contenido').classList.add('w-100');
     }
-
-  
-
-    
 }
